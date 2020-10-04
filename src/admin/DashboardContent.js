afd005cc8ba4fbbdc2cf332d8f829cf2563663d1
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-10-05 02:21:39 +0545

    second card done

--- src/admin/DashboardContent.js
+++ src/admin/DashboardContent.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './DashboardContent.css';
+import { Link } from 'react-router-dom';
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
 
 function DashboardContent() {
+    const classes = useStyles();
+
     return (
         <div className="dashboardContent">
             {/* Card */}
@@ -17,9 +35,36 @@ function DashboardContent() {
                 </span>
             </div>
 
-
-
             {/* Projects */}
+            <div className="dashboardContent__projects">
+                <div className="dashboardContent__projects--heading">
+                    <h3>Recent Projects</h3>
+                    <button>Post a Project</button>
+                </div>
+                <div className="dashboardContent__projects--table">
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Project / Contest Title</TableCell>
+                                    <TableCell align="right">Bids/Entries</TableCell>
+                                    <TableCell align="right">Average Bid</TableCell>
+                                    <TableCell align="right">Close Date</TableCell>
+                                    <TableCell align="right">Status</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableCell align="left"><EmojiEventsIcon /> <Link>WordPress Website </Link></TableCell>
+                                <TableCell align="right"> - </TableCell>
+                                <TableCell align="right"> - </TableCell>
+                                <TableCell align="right"> - </TableCell>
+                                <TableCell align="right">Inactive</TableCell>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+                <p><Link>View All <ArrowRightAltIcon /></Link></p>
+            </div>
             {/* News Feed */}
 
         </div>
